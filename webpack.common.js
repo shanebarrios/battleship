84161f535e0b6923ba953bc8e880fdb84b5e7ee6
shanebarrios <sbarrio1@terpmail.umd.edu>
2025-03-03 22:07:25 -0500

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,34 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __dirname = import.meta.dirname;
+
+export default {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+};
